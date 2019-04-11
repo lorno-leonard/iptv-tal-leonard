ec46292ee94f0961adbe513c138c2b458dbc3d84
Leonard Laput <lorno.leonard@gmail.com>
2019-04-11 14:54:37 +0800

    refactor: initialization

--- static/script/appui/components/main.js
+++ static/script/appui/components/main.js
@@ -2,37 +2,39 @@ define('iptv/appui/components/main', [
 	'antie/widgets/component',
 	'antie/widgets/button',
 	'antie/widgets/label',
+	'iptv/appui/widgets/keyboard',
 	'antie/storageprovider',
 	'iptv/appui/helpers/translate'
-], function(Component, Button, Label, StorageProvider, Translate) {
+], function(Component, Button, Label, Keyboard, StorageProvider, Translate) {
 	// All components extend Component
 	return Component.extend({
 		init: function init() {
-			var self = this;
-
-			// It is important to call the constructor of the superclass
 			init.base.call(this, 'maincomponent');
 
-			// // Get a reference to the current application and device objects
+			// Get a reference to the current application and device objects
 			this._application = this.getCurrentApplication();
 			this._device = this._application.getDevice();
 			this._storage = this._device.getStorage(StorageProvider.STORAGE_TYPE_PERSISTENT, 'iptv');
 
 			// Initialize tranlator
-			var locale = this._storage.getItem('locale') || 'en';
-			var t = new Translate(locale);
+			this._translator = null;
+			this._initTranslator();
 
-			// Add label to the component
-			var helloWorldLabel = new Label('helloWorldLabel', t.translate('Hello World'));
-			self.appendChildWidget(helloWorldLabel);
+			// Initialize widgets
+			this._keyboard = null;
+			this._initWidgets();
 
-			// Add button to the component
-			var testButton = new Button();
-			testButton.appendChildWidget(new Label(t.translate('Select me!')));
-			testButton.addEventListener('select', function() {
-				alert('I am selected!');
-			});
-			self.appendChildWidget(testButton);
+			// // Add label to the component
+			// var helloWorldLabel = new Label('helloWorldLabel', t.translate('Hello World'));
+			// self.appendChildWidget(helloWorldLabel);
+
+			// // Add button to the component
+			// var testButton = new Button();
+			// testButton.appendChildWidget(new Label(t.translate('Select me!')));
+			// testButton.addEventListener('select', function() {
+			// 	alert('I am selected!');
+			// });
+			// self.appendChildWidget(testButton);
 
 			// calls Application.ready() the first time the component is shown
 			// the callback removes itself once it's fired to avoid multiple calls.
@@ -40,6 +42,18 @@ define('iptv/appui/components/main', [
 			// 	self.getCurrentApplication().ready();
 			// 	self.removeEventListener('aftershow', appReady);
 			// });
+		},
+		_initTranslator: function _initTranslator() {
+			var locale = this._storage.getItem('locale') || 'en';
+			this._translator = new Translate(locale);
+		},
+		_initWidgets: function _initWidgets() {
+			this._addKeyboardWidget();
+		},
+		_addKeyboardWidget: function _addKeyboardWidget() {
+			var keyboard = new Keyboard('ipTvKeyboardContainer');
+			this._keyboard = keyboard;
+			this.appendChildWidget(keyboard);
 		}
 	});
 });
